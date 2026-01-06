2420d5404485d4deaea9d8fde6f3164233b8901d
CodeTile <CodeTile@dummy.com>
2026-01-06 11:18:55 +0000

    events now fire

--- FluentMultiSelect/FluentMultiSelect/Components/Pages/Shared/ChangeContext.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace FluentMultiSelect.Components.Pages.Shared;
-
-public sealed class SelectChangeContext
-{
-	public bool IsMulti { get; init; }
-	public string? Value { get; init; }
-	public IReadOnlyList<string>? Values { get; init; }
-}
