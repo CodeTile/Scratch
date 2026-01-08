583cb2614c9e60391a28cdad3c48ae1cf65b2541
CodeTile <CodeTile@dummy.com>
2026-01-08 08:55:33 +0000

    Initial project with checkbox

---
+++ FluentUI/FluentUI/Data/DataHelper.cs
@@ -0,0 +1,70 @@
+﻿using FluentUI.Data.Models;
+
+namespace FluentUI.Data
+{
+	public static class DataHelper
+	{
+		public static List<Employee> GetMockEmployees()
+		{
+			return
+			[
+				new()
+			{
+				EmployeeId = 4,
+				FirstName = "S Ravi",
+				LastName = "Kumar",
+				MobileNo = "9901091975",
+				Email = "SKumar@gmail.com"
+			},
+			new()
+				{
+					EmployeeId = 6,
+					FirstName = "Payal",
+					LastName = "Jain",
+					MobileNo = "9001091905",
+					Email = "PJain@gmail.com"
+				},
+			new()
+			{
+				EmployeeId = 7,
+				FirstName = "Alok",
+				LastName = "Ojha",
+				MobileNo = "900091905",
+				Email = "AOjha@gmail.com"
+			},
+			new()
+			{
+				EmployeeId = 9,
+				FirstName = "Divya",
+				LastName = "Bharti",
+				MobileNo = "9111767895",
+				Email = "DBharti@gmail.com"
+			},
+			new()
+				{
+					EmployeeId = 10,
+				FirstName = "Mayuri",
+				LastName = "Kango",
+				MobileNo = "9111000025",
+				Email = "MKango@gmail.com"
+			},
+			new()
+				{
+					EmployeeId = 11,
+				FirstName = "Tamraj",
+				LastName = "Kilwish",
+				MobileNo = "9251000025",
+				Email = "TKilwish@gmail.com"
+			},
+			new()
+				{
+					EmployeeId = 12,
+				FirstName = "James",
+				LastName = "Bond",
+				MobileNo = "9521000025",
+				Email = "James007@gmail.com"
+			}
+			];
+		}
+	}
+}
---
+++ FluentUI/FluentUI/Data/Models/Employee.cs
@@ -0,0 +1,26 @@
+﻿namespace FluentUI.Data.Models
+{
+	public class Employee
+	{
+		public long EmployeeId
+		{ get; set; }
+
+		public string FirstName
+		{ get; set; }
+
+		public string LastName
+		{ get; set; }
+
+		public string FullName
+		{
+			get
+			{
+				return FirstName + " " + LastName;
+			}
+		}
+		public string MobileNo
+		{ get; set; }
+		public string Email
+		{ get; set; }
+	}
+}
