d44a2e62b8966070b24ae2bf39d8df51584b7029
CodeTile <CodeTile@dummy.com>
2026-05-06 09:23:08 +0100

    Tidyup scratch

--- FluentUI/FluentUI.Tests.Reqnroll/StepDefinitions/TestableCheckBoxList.cs
+++
@@ -1,9 +0,0 @@
-using FluentUI.Components.Shared;
-
-namespace FluentUI.Components.Tests
-{// Subclass to expose protected OnParametersSet for testing
-	public class TestableCheckBoxList<TItem> : CheckBoxList<TItem>
-	{
-		public void OnParametersSetPublic() => base.OnParametersSet();
-	}
-}
--- Swagger10/Swagger10/Startup/ApiKeyMiddlewareExtensions.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Swagger10.Startup;
-
-public static class ApiKeyMiddlewareExtensions
-{
-	public static IApplicationBuilder UseApiKey(this IApplicationBuilder app)
-	{
-		return app.UseMiddleware<ApiKeyMiddleware>();
-	}
-}
